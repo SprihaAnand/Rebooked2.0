2ff8331d52a02c956d0c2f2c52a0eab8692d2af5
SprihaAnand <21bcs040@nith.ac.in>
2024-06-10 12:15:32 +0530

    after login register switch

--- client/src/components/Shared/Form/Form.js
+++ client/src/components/Shared/Form/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import InputType from './InputType';
 
-const Form = ({ submitBtn, formTitle }) => {
+const Form = ({formType, submitBtn, formTitle }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("donar");
@@ -17,70 +17,105 @@ const Form = ({ submitBtn, formTitle }) => {
       <form>
         <h1 className='text-center'> {formTitle}</h1>
         <hr/>
-        <InputType 
-          labelText={'Email'}
-          labelFor={'forEmail'}
-          inputType={'email'}
-          name={'email'}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <InputType 
-          labelText={'Password'}
-          labelFor={'forPassword'}
-          inputType={'password'}
-          name={'password'}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <InputType
-          labelText={"Name"}
-          labelFor={"forName"}
-          inputType={"text"}
-          name={"name"}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <InputType
-          labelText={"Organisation Name"}
-          labelFor={"fororganisationName"}
-          inputType={"text"}
-          name={"organisationName"}
-          value={organisationName}
-          onChange={(e) => setOrganisationName(e.target.value)}
-        />  
-        <InputType
-          labelText={"Institute Name"}
-          labelFor={"forInstituteName"}
-          inputType={"text"}
-          name={"instituteName"}
-          value={instituteName}
-          onChange={(e) => setInstituteName(e.target.value)}
-        />  
-        <InputType
-          labelText={"Website"}
-          labelFor={"forWebsite"}
-          inputType={"text"}
-          name={"website"}
-          value={website}
-          onChange={(e) => setWebsite(e.target.value)}
-        />  
-        <InputType
-          labelText={"Address"}
-          labelFor={"forAddress"}
-          inputType={"text"}
-          name={"address"}
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        <InputType
-          labelText={"Phone"}
-          labelFor={"forPhone"}
-          inputType={"text"}
-          name={"phone"}
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-        />
+        {/* switch statement */}
+        {(()=>{
+          //eslint-disable-next-line
+          switch(true){
+            case formType === 'login':{
+                return (
+                  <>
+                    <InputType 
+                      labelText={'Email'}
+                      labelFor={'forEmail'}
+                      inputType={'email'}
+                      name={'email'}
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <InputType 
+                      labelText={'Password'}
+                      labelFor={'forPassword'}
+                      inputType={'password'}
+                      name={'password'}
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </>
+                );
+            }
+          case formType === 'register':{
+            return(
+              <>
+                <InputType 
+                  labelText={'Email'}
+                  labelFor={'forEmail'}
+                  inputType={'email'}
+                  name={'email'}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <InputType 
+                  labelText={'Password'}
+                  labelFor={'forPassword'}
+                  inputType={'password'}
+                  name={'password'}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputType
+                  labelText={"Name"}
+                  labelFor={"forName"}
+                  inputType={"text"}
+                  name={"name"}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <InputType
+                  labelText={"Organisation Name"}
+                  labelFor={"fororganisationName"}
+                  inputType={"text"}
+                  name={"organisationName"}
+                  value={organisationName}
+                  onChange={(e) => setOrganisationName(e.target.value)}
+                />  
+                <InputType
+                  labelText={"Institute Name"}
+                  labelFor={"forInstituteName"}
+                  inputType={"text"}
+                  name={"instituteName"}
+                  value={instituteName}
+                  onChange={(e) => setInstituteName(e.target.value)}
+                />  
+                <InputType
+                  labelText={"Website"}
+                  labelFor={"forWebsite"}
+                  inputType={"text"}
+                  name={"website"}
+                  value={website}
+                  onChange={(e) => setWebsite(e.target.value)}
+                />  
+                <InputType
+                  labelText={"Address"}
+                  labelFor={"forAddress"}
+                  inputType={"text"}
+                  name={"address"}
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+                <InputType
+                  labelText={"Phone"}
+                  labelFor={"forPhone"}
+                  inputType={"text"}
+                  name={"phone"}
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </>
+            )
+          }
+        }
+        })()}
+
         <div className='d-flex'>
           <button type="submit" className="btn btn-primary">{submitBtn}</button>
         </div>
