373a7cf6cd90f56ec04db9b0f7f4c822476eb8e3
SprihaAnand <21bcs040@nith.ac.in>
2024-06-13 19:16:08 +0530

    css for admin home finalised

--- client/src/pages/Admin/AdminHome.js
+++ client/src/pages/Admin/AdminHome.js
@@ -1,23 +1,49 @@
 import React from "react";
 import Layout from "../../components/Shared/Layout/Layout";
 import { useSelector } from "react-redux";
-
+import './adminhome.css'
 const AdminHome = () => {
   const { user } = useSelector((state) => state.auth);
   return (
     <Layout>
-      <div className="container">
-        <div className="d-felx flex-column mt-4">
-          <h1>
-            Welcome Admin <i className="text-success">{user?.name}</i>
-          </h1>
-          <h3>Rebooked </h3>
-          <hr />
-          <p>
-          Without a middleman or inventory, we want to build a platform that connects people who want to trade, buy, sell, or donate books. By collaborations with school and college administration, we seek to spread the word about the accessibility of low-cost books and emphasis the value of education and literacy. By making books more accessible and inexpensive, we hope to increase literacy rates and equip kids with the skills they need to excel in the classroom and beyond. Our platform encourages a more ecologically friendly approach to book consumption while being a cost-effective and sustainable means to provide access to books. We intend to reduce waste by enticing people to reuse and recycle books rather than buy new ones encourage everyone to live in a sustainable future.Overall, we are enthusiastic about the project's potential effects, the chance to increase disadvantaged school children' access to affordable books, and the chance to enhance their education. We want to leave a positive legacy that people and communities can take advantage of for years to come.
-          </p>
-        </div>
-      </div>
+ <div className="start">
+
+<body>
+  
+   <div className="intro-header">
+       <div className="container">
+
+           <div className="row">
+               <div className="col-lg-8">
+                   <div className="intro-message">   
+                       <h1 className="animated slideInLeft">"Books are a uniquely portable magic"<br></br>
+                       <span className="highlight"> — Stephen King</span>.</h1>
+                     <h3 className="animated slideInRight">Know more about this project.</h3>
+                     
+                         <ul className="list-inline intro-social-buttons">
+                           <li>
+                                <a href="#"><button className="button animated bounceInUp btn btn-info btn-lg"> <span className="network-name">Github</span></button></a>
+                           </li>
+                           
+                           
+                       </ul>
+                    
+                    
+                     
+             </div> 
+               </div>
+             <div className="col-lg-4"></div>
+           </div>
+
+       </div>
+
+   
+   </div>
+  
+</body>
+</div>
+     
+
     </Layout>
   );
 };
